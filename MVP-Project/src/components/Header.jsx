006c2c805df4b81e0ad4c4730f1af318c6c3d96e
Mohammed Tawfiq Iddrisu <84660782+Tawphiq@users.noreply.github.com>
2023-04-04 06:23:28 +0100

    "navbar"

--- MVP-Project/src/components/Header.jsx
+++ MVP-Project/src/components/Header.jsx
@@ -5,9 +5,9 @@ import logo1 from './images/imageGame.jpg'
 const Header = () => {
   return (
     <header className='grid grid-cols-2'>
-      <img src = {logo1} alt=""  className='h-12 w-24 cursor-pointer'/>
+      <img src = {logo1} alt=""  className='w-24 h-12 cursor-pointer'/>
       <nav>
-        <ul className='grid-cols-3 text-white hidden text-2xl font-bold'>
+        <ul className='justify-end hidden grid-cols-3 gap-4 text-2xl font-bold text-white md:flex'>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
